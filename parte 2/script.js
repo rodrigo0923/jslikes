4acb4dd4629573728c543c33611dbf6aac8aa4f9
rodrigo0923 <rodrigo.yanez1401@gmail.com>
2023-09-20 09:15:38 -0300

    likes

---
+++ parte 2/script.js
@@ -0,0 +1,24 @@
+var clicks = 0;
+var clicks2 = 0;
+var clicks3 = 0;
+    function myFunction() {
+
+        clicks += 1;
+        document.getElementById("demo").innerHTML = clicks + "Likes";
+
+
+    }
+    function myFunction2() {
+
+        clicks2 += 1;
+        document.getElementById("demo2").innerHTML = clicks2 + "Likes";
+
+
+    }
+    function myFunction3() {
+
+        clicks3 += 1;
+        document.getElementById("demo3").innerHTML = clicks3 + "Likes";
+
+
+    }
---
+++ script.js
@@ -0,0 +1,8 @@
+var clicks = 0;
+    function myFunction() {
+
+        clicks += 1;
+        document.getElementById("demo").innerHTML = clicks + "Likes";
+
+
+    }
